ac760208d44189fb177841251835612a80e1b6be
satish.agarwal <satish.agarwal@olacabs.com>
2020-05-01 22:19:27 +0530

    login implementation

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: java -jar /Users/satish.agarwal/cookservice/target/cook-service-1.0-SNAPSHOT.jar
+port: process.env.PORT || 8081
--- src/main/java/com/cook/authentication/repository/CookRepository.java
+++ src/main/java/com/cook/authentication/repository/CookRepository.java
@@ -1,10 +1,10 @@
-package com.cook.authentication.repository;
-
-import com.cook.authentication.model.CookEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CookRepository extends JpaRepository<CookEntity, String> {
-
-    CookEntity findByCookId(String cookId);
-
-}
+//package com.cook.authentication.repository;
+//
+//import com.cook.authentication.model.CookEntity;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface CookRepository extends JpaRepository<CookEntity, String> {
+//
+//    CookEntity findByCookId(String cookId);
+//
+//}
